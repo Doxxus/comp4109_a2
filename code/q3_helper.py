7c65c6bc75029270af4ae7494c050fe556ad383f
Doxxus <lockycampbell@cmail.carleton.ca>
2019-11-11 16:00:06 -0500

    partial solution to Q3... just need to figure out how to convert i2osp to an integer

--- code/q3_helper.py
+++ code/q3_helper.py
@@ -3,11 +3,18 @@ import hashlib
 def i2osp(integer, size=4):
     return ''.join([chr((integer >> (8 * i)) & 0xFF) for i in reversed(range(size))])
 
-def mgf1(input, length, hash=hashlib.sha1):
+def h(x):
+    return str((2*x + 1) % 65521)
+
+def mgf1(input, length):
     counter = 0
     output = ''
     while (len(output) < length):
         C = i2osp(counter, 4)
-        output += hash(input + C).digest()
+        output += h(int(input + int(C)))
         counter += 1
     return output[:length]
+    
+num = ((2**7) + (2**9) + (2**10))
+
+print(str(mgf1(num, 5)))
