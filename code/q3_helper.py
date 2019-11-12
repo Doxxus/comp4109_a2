99d677783934031456f8aef850c3c09bdc960317
Doxxus <lockycampbell@cmail.carleton.ca>
2019-11-12 16:15:18 -0500

    added the shares.txt and rsa.txt to the repo

--- code/q3_helper.py
+++ code/q3_helper.py
@@ -1,20 +1,21 @@
 import hashlib
-
-def i2osp(integer, size=4):
-    return ''.join([chr((integer >> (8 * i)) & 0xFF) for i in reversed(range(size))])
+from binascii import hexlify
 
 def h(x):
-    return str((2*x + 1) % 65521)
+    return (2*x + 1) % 65521
 
-def mgf1(input, length):
-    counter = 0
-    output = ''
-    while (len(output) < length):
-        C = i2osp(counter, 4)
-        output += h(int(input + int(C)))
-        counter += 1
-    return output[:length]
+def i2osp(integer, size=4):
+  return ''.join([chr((integer >> (8 * i)) & 0xFF) for i in reversed(range(size))])
+
+def mgf1(input, length, hash=hashlib.sha1):
+  counter = 0
+  output = ''
+  while (len(output) < length):
+    C = i2osp(counter, 4)
+    output += hash(input + C).digest()
+    counter += 1
+  return output[:length]
     
 num = ((2**7) + (2**9) + (2**10))
 
-print(str(mgf1(num, 5)))
+print(hexlify(mgf1(str(num), 5)))
